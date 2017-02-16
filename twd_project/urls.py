74ff6c682328e1bffbe3b09922d910175dd2bdff
ZC44 <2227781C@student.gla.ac.uk>
2017-02-16 20:13:03 +0000

    Chapter 5 tbc

--- twd_project/urls.py
+++ twd_project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from rango import views
@@ -21,4 +23,5 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/about/', views.about),
+    url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
