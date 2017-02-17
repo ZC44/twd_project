d573c650bddb750c1007ff6ee2afb6266cea050b
ZC44 <2227781C@student.gla.ac.uk>
2017-02-17 16:53:02 +0000

    Chapter 7

--- rango/urls.py
+++ rango/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
-from django.conf.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^rango/', include('rango.urls')),
-    url(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category,name='show_category'),
+]
