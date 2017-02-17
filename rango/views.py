17334760099829e3cbe5677c97744c95cdb50bbc
ZC44 <2227781C@student.gla.ac.uk>
2017-02-17 15:45:57 +0000

    Chapter 6 tbc2

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rango.models import Category,Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories':category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories':category_list,'pages':page_list}
     return render(request,'rango/index.html',context_dict)
 
 def about(request): return render(request,'rango/about.html')
